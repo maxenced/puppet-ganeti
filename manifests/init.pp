c5903b6cc610e541c1574b430b9b9e9796848c89
root <root@palo.infra.ubuntu-fr.org>
2012-07-17 21:45:41 +0200

    fix ganeti include

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,42 @@
-import "classes/*.pp"
+import "classes/clocksource.pp"
+import "classes/drbd.pp"
+import "classes/ganeti.pp"
+import "classes/hooks.pp"
+import "classes/kvm.pp"
+import "classes/ntp.pp"
+import "classes/xenbridge.pp"
+class ganeti {
+    package { 'ganeti2': ensure => installed, }
+
+    $hostline = inline_template('<%= ipaddress %>	<%= fqdn %> <%= hostname %>')
+
+    exec { "/usr/bin/perl -pi -e 's/^$ipaddress.*/$hostline/' /etc/hosts":
+        unless => "/bin/grep -q '$hostline' /etc/hosts",
+    }
+
+    file { '/etc/hostname':
+        content => "$fqdn\n",
+        notify  => Service['hostname'],
+    }
+
+    file { '/etc/lvm/lvm.conf':
+        source  => 'puppet:///ganeti/lvm/lvm.conf',
+    }
+
+    service { 'hostname': }
+
+    case $hypervisor {
+        "xen" : {
+            include ganeti::xen
+        }
+        "kvm" : {
+            include ganeti::kvm
+        }
+    }
+    include ganeti::drbd
+    include ganeti::xenbridge
+}
+
+mport "classes/xen.pp"
 
 # vim: set ts=4 sw=4 et:
